c6355aceec1eea0cd20252466fa1e87f200ccc6c
Grace Choi <oiograceoio@gmail.com>
2019-04-18 22:12:15 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,7 +6,7 @@ class SchoolClassesController < ApplicationController
   def create
     @school_class = SchoolClass.new(school_params)
     @school_class.save
-    redirect_to schoolclass_path(@school_class)
+    redirect_to school_class_path(@school_class)
   end
 
   def show
@@ -20,7 +20,7 @@ class SchoolClassesController < ApplicationController
   def update
     @school_class = SchoolClass.find(params[:id])
     @school_class.update(school_params)
-    redirect_to schoolclass_path(@school_class)
+    redirect_to school_class_path(@school_class)
   end
 
   private
